28ad72d517b959a82e58763d15dd2bcaa4170e4f
ryanknights <hello@ryanknights.co.uk>
2016-03-30 21:43:02 +0100

    Added placeholder files for manipulation & traversing methods

--- src/library.js
+++ src/library.js
@@ -7,10 +7,18 @@ import {_, Library} from "./modules/core.js";
 import {displayMethods} from "./modules/display.js";
 import {classMethods} from "./modules/class.js";
 import {eventMethods} from "./modules/events.js";
+import {manipulationMethods} from "./modules/manipulation.js";
+import {traversingMethods} from "./modules/traversing.js";
 
 
 /*----------  Extend our librarys prototype with above modules  ----------*/
-Object.assign(Library.prototype, displayMethods, classMethods, eventMethods);
+Object.assign(Library.prototype, 
+	displayMethods, 
+	classMethods, 
+	eventMethods,
+	manipulationMethods,
+	traversingMethods
+);
 
 /*----------  Assign our library call function onto the window  ----------*/
 if (window._ === undefined)
---
+++ src/modules/traversing.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const traversingMethods =
+{
+
+}
+
+export { traversingMethods };
